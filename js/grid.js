e599a07e6263cea3f3c78ef7e9457713c311b0a6
omoskalenko <moskalenkogm@gmail.com>
2019-01-24 10:54:57 +0300

    Объявлены свойства и состояние класса Grid

---
+++ js/grid.js
@@ -0,0 +1,8 @@
+class Grid {
+  constructor(gridWidth, gridHight, gridRows, gridCols) {
+    this.gridWidth = gridWidth;
+    this.gridHight = gridHight;
+    this.gridRows = gridRows;
+    this.gridCols = gridCols;
+  }
+}
