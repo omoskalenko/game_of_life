91467f62638b394b320961367afee3ed9e5d53e7
omoskalenko <moskalenkogm@gmail.com>
2019-01-24 11:25:10 +0300

    Создан метод Init для инициализации ячейки в виде DOM элемента

--- js/cell.js
+++ js/cell.js
@@ -3,5 +3,14 @@ class Cell {
     this.row = row;
     this.col = col;
     this.isAlive = alive;
+    this.element = null;
+
+    this.init();
+  }
+
+  init() {
+    const td = document.createElement('td');
+    td.className = 'cell';
+    this.element = td;
   }
 }
