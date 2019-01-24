887f13c66f1b232b88e129c514add01776bbf701
omoskalenko <moskalenkogm@gmail.com>
2019-01-24 13:14:22 +0300

    Созданы элементы управления

--- js/game.js
+++ js/game.js
@@ -1,4 +1,4 @@
-/*global Grid */
+/*global Grid, createButton, createElement, carry*/
 
 class Game {
   constructor(gridWidth, gridHeight, gridRows, gridCols, rootElement) {
@@ -11,7 +11,7 @@ class Game {
     this.grid = new Grid(gridWidth, gridHeight, gridRows, gridCols); //композиция
     this.isPlaying = false;
     this.speed = 1000;
-    this.interval = null; 
+    this.interval = null;
     this.element = null;
 
     this.init();
@@ -19,6 +19,35 @@ class Game {
 
   init() {
     this.root.appendChild(this.grid.element);
+    this.root.appendChild(this._createControls());
+    
   }
 
+  _createControls() {
+    const startButton = createButton({
+      className: 'material-icons',
+      textContent: 'play_arrow'
+    });
+    const resetButton = createButton({
+      className: 'material-icons',
+      textContent: 'replay'
+    });
+    const randomizeButton = createButton({
+      className: 'material-icons',
+      textContent: 'transform'
+    });
+    const speedSlider = createElement('input', {
+      type: 'range',
+      min: 0,
+      max: 900,
+      step: 50
+    });
+    const container = createElement('div', {
+      className: 'controls'
+    });
+
+    container.append(startButton, resetButton, randomizeButton, speedSlider);
+
+    return container;
+  }
 }
