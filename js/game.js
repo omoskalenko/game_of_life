5eedd3eaee52fadf712c4ed2a4fcd8442edc347b
omoskalenko <moskalenkogm@gmail.com>
2019-01-24 10:51:45 +0300

    Объявлены свойства и состояние класса Game

--- js/game.js
+++ js/game.js
@@ -1,8 +1,17 @@
+/*global Grid */
+
 class Game {
   constructor(gridWidth, gridHight, gridRows, gridCols, rootElement) {
     this.gridWidth = gridWidth;
     this.gridHight = gridHight;
     this.gridRows = gridRows;
     this.gridCols = gridCols;
+
+    this.grid = new Grid(gridWidth, gridHight, gridRows, gridCols); //композиция
+    this.isPlaying = false;
+    this.speed = 1000;
+    this.interval = null; 
+    this.element = rootElement;
   }
+
 }
