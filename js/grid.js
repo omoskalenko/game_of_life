8afd3e3fc6d1d4bd503ead4a438d7ed98152c050
omoskalenko <moskalenkogm@gmail.com>
2019-01-24 11:03:30 +0300

    Объявлен метод init для инициализациии экземпляра сетки

--- js/grid.js
+++ js/grid.js
@@ -1,8 +1,24 @@
+/*global Cell */
 class Grid {
   constructor(gridWidth, gridHight, gridRows, gridCols) {
     this.gridWidth = gridWidth;
     this.gridHight = gridHight;
     this.gridRows = gridRows;
     this.gridCols = gridCols;
+
+    this.grid = [];
+    this.nextGrid = [];
+  }
+
+  init() {
+    for (let i = 0; i < this.gridRows; i++) {
+      this.grid[i] = [];
+      this.nextGrid[i] = [];
+      for (let j = 0; j < this.gridCols; j++) {
+        this.this.grid[i][j] = new Cell(i, j);
+        this.this.nextGrid[i][j] = false;
+      }
+      
+    }
   }
 }
